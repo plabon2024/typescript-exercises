d513b17ae0ca1ac9bec80ba1a2e7a25f74f9f5ad
sheeedplabon <shaeedplabon@gmail.com>
2025-11-18 17:51:19 +0600

    clean up

--- solution.ts
+++ solution.ts
@@ -1,7 +1,7 @@
-// ** Problem 1
 
-type TProblem1 = string | number | boolean;
-function formatValue(x: TProblem1): TProblem1 {
+
+type TValue = string | number | boolean;
+function formatValue(x: TValue): TValue {
     if (typeof x === "string") {
         return x.toUpperCase();
     } else if (typeof x === "number") {
@@ -12,11 +12,9 @@ function formatValue(x: TProblem1): TProblem1 {
         return x;
     }
 }
-console.log(formatValue("hello"));
-console.log(formatValue(5));
-console.log(formatValue(true));
 
-// ** Problem 2
+
+
 
 type TProblem2 = string | unknown[];
 function getLength(x: TProblem2): number {
@@ -29,23 +27,17 @@ function getLength(x: TProblem2): number {
     }
 }
 
-console.log(getLength("typescript"));
-console.log(getLength([10, 20, 30, 40]));
 
-// ** Problem 3
+
+
 class Person{
     constructor(public name: string, public age: number){}
     getDetails() {
-    return `Name: ${this.name}, Age: ${this.age}`;
+    return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
 
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
 
-// ** Problem 4
 interface TBook{
     title:string;
     rating:number;
@@ -54,13 +46,10 @@ function filterByRating(books:TBook[]):TBook[]{
     return books.filter((book)=>book.rating>=4)
 
 }
-const books = [
-  { title: 'Book A', rating: 4.5 },
-  { title: 'Book B', rating: 3.2 },
-  { title: 'Book C', rating: 5.0 },
-];
-console.log(filterByRating(books));
-// ** Problem 5
+
+
+
+
 interface TUser{
     name:string;
     email:string;
@@ -69,15 +58,11 @@ interface TUser{
 function filterActiveUsers(users:TUser[]):TUser[]{
     return users.filter((user) => user.isActive);
 
-}const users = [
-  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-];
+}
 
-console.log(filterActiveUsers(users));
 
-// ** Problem 6
+
+
 interface Book {
     title: string;
     author: string;
@@ -87,15 +72,9 @@ interface Book {
 function printBookDetails(book: Book):void {
     console.log(`Title: ${book.title}, Author:${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable?"Yes":"No"}`)
 }
-const myBook: Book = {
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald',
-  publishedYear: 1925,
-  isAvailable: true,
-};
 
-printBookDetails(myBook);
-// ** Problem 7
+
+
 type TArray = (string | number)[];
 function getUniqueValues(array1: TArray, array2: TArray): TArray {
     const uniqueArray: TArray = [];
@@ -127,18 +106,15 @@ function getUniqueValues(array1: TArray, array2: TArray): TArray {
     return uniqueArray;
 }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-console.log(getUniqueValues(array1, array2));
 
-// ** Problem 8
+
+
 interface TProduct {
     name: string;
     price: number;
     quantity: number;
     discount?: number;
 }
-
 function calculateTotalPrice(products: TProduct[]) {
 
     if (products.length !== 0) {
@@ -152,10 +128,4 @@ function calculateTotalPrice(products: TProduct[]) {
     }
 }
 
-const products = [
-    { name: 'Pen', price: 10, quantity: 2 },
-    { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-    { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-];
 
-console.log(calculateTotalPrice(products));
